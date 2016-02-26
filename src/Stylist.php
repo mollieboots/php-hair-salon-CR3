0632e7f3acdcf2b74dba42f27a8844cba7b99782
mollieboots <student@epicodus.com>
2016-02-26 08:27:27 -0800

    tests for getname and getid pass

---
+++ src/Stylist.php
@@ -0,0 +1,29 @@
+<?php
+    class Stylist
+    {
+        private $name;
+        private $id;
+
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function setName($name)
+        {
+            $this->name = $name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+    }
+
+ ?>
---
+++ tests/StylistTest.php
@@ -0,0 +1,44 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Stylist.php";
+
+    $server = 'mysql:host=localhost;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+        function test_getName()
+        {
+            //Arrange
+            $name = "Lizzy";
+            $test_stylist = new Stylist($name);
+
+            //Act
+            $result = $test_stylist->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+        function test_getId()
+        {
+            //Arrange
+            $name = "Monica";
+            $id = 3;
+            $test_stylist = new Stylist($name, $id);
+
+            //Act
+            $result = $test_stylist->getId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+    }
+ ?>
